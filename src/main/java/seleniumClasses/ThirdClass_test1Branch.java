9b5b667f922209fbc0c25f81ab6c6c768da733be
Yuonus <syuonus@gmail.com>
2025-05-01 10:17:38 -0400

    Adding the code to the first feature branch

---
+++ src/main/java/seleniumClasses/ThirdClass_test1Branch.java
@@ -0,0 +1,10 @@
+package seleniumClasses;
+
+public class ThirdClass_test1Branch {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Not quitting is the key to success.");
+	}
+
+}
