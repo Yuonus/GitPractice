eddf57e8abba85ec539d46dc70f0a5b7a2f00ba7
Yuonus <syuonus@gmail.com>
2025-05-01 10:27:26 -0400

    The CR comments were taken into the consideration

--- src/main/java/seleniumClasses/ThirdClass_test1Branch.java
+++ src/main/java/seleniumClasses/ThirdClass_test1Branch.java
@@ -5,6 +5,7 @@ public class ThirdClass_test1Branch {
 	public static void main(String[] args) {
 		
 		System.out.println("Not quitting is the key to success.");
+		// Added the by locators
 	}
 
 }
