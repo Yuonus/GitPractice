e7d1a4853127da6f283a8d1906284b5d0d4de0ea
Yuonus <syuonus@gmail.com>
2025-05-01 08:56:22 -0400

    First Commit of the project

---
+++ src/main/java/seleniumClasses/FirstClass.java
@@ -0,0 +1,16 @@
+package seleniumClasses;
+
+public class FirstClass {
+	
+	public static void main(String[] args) {
+		
+//		WebDriver driver = new ChromeDriver();
+		
+//		Practice
+		System.out.println("I am practicing git");
+		
+		
+		
+	}
+
+}
