281f8db20c32bf5abb89779ced3b6bb36c6f087b
Yuonus <syuonus@gmail.com>
2025-05-01 15:54:50 -0400

    The second version of the app is ready to be tagged

---
+++ src/main/java/seleniumClasses/Feaure2.java
@@ -0,0 +1,11 @@
+package seleniumClasses;
+
+public class Feaure2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// This is feature Two of the code
+
+	}
+
+}
