8cf06b61f9e93a727c84145f4277087c4ce1c65f
Yuonus <syuonus@ndc.nasa.gov>
2026-02-24 11:38:28 -0500

    The second change was added successfully

---
+++ src/nationWideMotorsLLC/S_Tech.java
@@ -0,0 +1,10 @@
+package nationWideMotorsLLC;
+
+public class S_Tech {
+
+	public static void main(String[]args) {
+		StringBuilder sb = new StringBuilder("CBA");
+		sb.reverse();
+		System.out.println(sb);
+	}
+}
