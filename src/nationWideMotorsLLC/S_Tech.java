bd912601d46e2211899d022297b4c018ec7a835d
Yuonus <syuonus@ndc.nasa.gov>
2026-02-26 12:23:39 -0500

    added

--- src/nationWideMotorsLLC/S_Tech.java
+++ src/nationWideMotorsLLC/S_Tech.java
@@ -3,9 +3,8 @@ package nationWideMotorsLLC;
 public class S_Tech {
 
 	public static void main(String[]args) {
-		StringBuilder sb = new StringBuilder("CBA");
-		sb.reverse();
-		System.out.println(sb);
-		System.out.println("You dont need to use the toString() method");
+
+
+
 	}
 }
