e12dd1855a0afc601784f0b6e406589ca8348f07
Yuonus <syuonus@ndc.nasa.gov>
2026-02-26 14:10:30 -0500

    Replicating the merge conflict scenario

---
+++ src/nationWideMotorsLLC/DemoPage.java
@@ -0,0 +1,12 @@
+package nationWideMotorsLLC;
+
+public class DemoPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enjoying working with Java programming language");
+
+	}
+
+}
